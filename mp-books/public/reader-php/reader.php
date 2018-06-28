8ffdb500ee8221146ddd5de42c11110aa9bc4b5a
Will Roscoe <1784940+willroscoe@users.noreply.github.com>
2018-06-28 12:36:20 +0100

    Added Matomo tracking

--- mp-books/public/reader-php/reader.php
+++ mp-books/public/reader-php/reader.php
@@ -84,4 +84,9 @@ get_header(); ?>
     .epub span.selected { display: inline; padding:10px 20px; margin:0px; line-height:2; }
 </style>
 
+<!-- Track event -->
+<script type="text/javascript">
+_paq.push(['trackEvent', 'Read Online', '<?php echo $basepath_read . '/' . $asset_to_process ?>']);
+</script>
+
 <?php get_footer(); ?>
